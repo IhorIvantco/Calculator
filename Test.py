26d83be0645fcf16a23edfb670535148c6230172
IhorIvantco <91619730+IhorIvantco@users.noreply.github.com>
2021-10-02 15:06:29 +0300

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,12 @@
+a = float(input("First number "))
+b = float(input("Second number "))
+operation = input("Input operation ")
+
+if operation == '+':
+    print(a+b)
+elif operation == '-':
+    print(a-b)
+elif operation == '*':
+    print(a*b)
+elif operation == '/':
+    print(a/b)
